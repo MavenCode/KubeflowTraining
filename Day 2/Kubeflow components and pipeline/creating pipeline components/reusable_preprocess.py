1f9b342a795df26b69ba6f2fd601374a50ba718c
Similoluwa Adelore <62348760+AdeloreSimiloluwa@users.noreply.github.com>
2021-02-18 10:57:37 +0000

    Add files via upload

---
+++ Day 2/Kubeflow components and pipeline/creating pipeline components/reusable_preprocess.py
@@ -0,0 +1,49 @@
+#importing libraries
+def preprocess():
+    #importing libraries
+    import joblib
+    import pandas as pd
+    import numpy as np
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.preprocessing import OneHotEncoder
+    from sklearn.model_selection import train_test_split
+    from sklearn.preprocessing import StandardScaler 
+    
+    #importing the data
+    data = pd.read_csv("https://raw.githubusercontent.com/MavenCode/KubeflowTraining/master/Data/Churn_Modelling.csv")
+    #dropping some columns that are not needed
+    data = data.drop(columns=['RowNumber','CustomerId','Surname'], axis=1)
+    #data features
+    X = data.iloc[:,:-1]
+    #target data
+    y = data.iloc[:,-1:]   
+    #encoding the categorical columns
+    le = LabelEncoder()
+    ohe = OneHotEncoder()
+    X['Gender'] = le.fit_transform(X['Gender'])
+    geo_df = pd.DataFrame(ohe.fit_transform(X[['Geography']]).toarray())
+
+    #getting feature name after onehotencoding
+    geo_df.columns = ohe.get_feature_names(['Geography'])
+
+    #merging geo_df with the main data
+    X = X.join(geo_df) 
+    #dropping the old columns after encoding
+    X.drop(columns=['Geography'], axis=1, inplace=True)
+
+    #splitting the data 
+    X_train,X_test,y_train,y_test = train_test_split( X,y, test_size=0.2, random_state = 42)
+    #feature scaling
+    sc =StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+
+    #saving output file to path
+    np.save('X_train.npy', X_train)
+    np.save('X_test.npy', X_test)
+    np.save('y_train.npy', y_train)
+    np.save('y_test.npy', y_test)
+
+#defining and parsing arguments
+if __name__ == '__main__':
+    preprocess()
