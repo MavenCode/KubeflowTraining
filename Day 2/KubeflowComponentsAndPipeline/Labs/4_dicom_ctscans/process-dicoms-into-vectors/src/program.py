dd28a57fbf68ae8c8afe1baf53538b5e082d9f56
charles adetiloye <charles@mavencode.com>
2021-02-25 23:29:18 -0600

    adding chicago taxi fix

--- Day 2/KubeflowComponentsAndPipeline/Labs/4_dicom_ctscans/process-dicoms-into-vectors/src/program.py
+++ Day 2/KubeflowComponentsAndPipeline/Labs/4_dicom_ctscans/process-dicoms-into-vectors/src/program.py
@@ -27,36 +27,45 @@ args = parser.parse_args()
 
 
 def create_3d_matrix(path):
-    dicoms = [pydicom.dcmread(f"{path}/{f}") for f in listdir(path)]
+    dicoms = [pydicom.dcmread(f"{path}/{f}", force=True) for f in listdir(path)]
     slices = [d for d in dicoms if hasattr(d, "SliceLocation")]
-    slices = sorted(slices, key=lambda s: s.SliceLocation)
-    ps = slices[0].PixelSpacing
-    ss = slices[0].SliceThickness
-    ax_aspect = ps[1] / ps[0]
-    sag_aspect = ps[1] / ss
-    cor_aspect = ss / ps[0]
+    if len(slices) > 0:
+        slices = sorted(slices, key=lambda s: s.SliceLocation)
+        ps = slices[0].PixelSpacing
+        ss = slices[0].SliceThickness
+        ax_aspect = ps[1] / ps[0]
+        sag_aspect = ps[1] / ss
+        cor_aspect = ss / ps[0]
 
-    # create 3D array
-    img_shape = list(slices[0].pixel_array.shape)
-    img_shape.append(len(slices))
-    img3d = np.zeros(img_shape)
+        # create 3D array
+        img_shape = list(slices[0].pixel_array.shape)
+        img_shape.append(len(slices))
+        img3d = np.zeros(img_shape)
 
-    for i, s in enumerate(slices):
-        img2d = s.pixel_array
-        img3d[:, :, i] = img2d
+        for i, s in enumerate(slices):
+            img2d = s.pixel_array
+            img3d[:, :, i] = img2d
 
-    return {
-        "img3d": img3d,
-        "img_shape": img_shape,
-        "ax_aspect": ax_aspect,
-        "sag_aspect": sag_aspect,
-        "cor_aspect": cor_aspect
-    }
+        return {
+            "img3d": img3d,
+            "img_shape": img_shape,
+            "ax_aspect": ax_aspect,
+            "sag_aspect": sag_aspect,
+            "cor_aspect": cor_aspect
+        }
+    else:
+        return {
+            "img3d": [],
+            "img_shape": [],
+            "ax_aspect": [],
+            "sag_aspect": [],
+            "cor_aspect": []
+        }
 
 
 input_dir = args.input_dir
 output_file = args.output_file
 
 m = create_3d_matrix(f"{input_dir}")
-np.savetxt('/mnt/data/converted_dicom.csv', m['img3d'].reshape((-1, m['img_shape'][2])), delimiter=",")
+#np.savetxt('/mnt/data/converted_dicom.csv', m['img3d'].reshape((-1, m['img_shape'][2])), delimiter=",")
 # upload_blob(args.bucket_name, "/tmp/s.csv", output_file)
